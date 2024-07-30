f3aabacd06408e79c61cb6f78feb8f580724230b
Daman2462 <22103069@mail.jiit.ac.in>
2024-07-30 15:48:45 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <iostream>
+int main(){
+int n;
+vector<double>arr;
+cout<<"Enter number of elements: ";
+cin>>n;
+
+for(int i=0;i<n;i++){
+double c;
+double ans=0;
+cout<<<"Enter:";
+cin>>c;
+arr.push_back(c);
+ans+=c;
+}
+cout<<ans/n<<endl;
+return 0;
+}
